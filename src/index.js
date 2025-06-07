aa87367afe40a4d3d82943c46560ccf193a543b2
unluna <unluna@unlunadeMacBook-Pro.local>
2025-06-07 16:24:38 +0800

    init: first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+	async fetch(request, env, ctx) {
+		const json = {
+			test: 'hello world!'
+		};
+		return new Response(JSON.stringify(json), {
+			status: 200,
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+	}
+};
